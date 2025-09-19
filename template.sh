dfcccf8c165996772602900d95870cd72edc1673
AritraBanerjee-09 <aritrab16118@gmail.com>
2025-09-20 02:22:40 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,15 @@
+# create dir
+mkdir -p src
+mkdir -p research
+
+# create files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch app.py
+touch setup.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "Directory and files are created!. "
